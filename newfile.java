89b85c037eb514f08b7bf8b892fc8d4ae95ed04b
dhiraj <dy0314864@gmail.com>
2025-01-17 05:13:13 +0000

    commit3

--- newfile.java
+++ newfile.java
@@ -2,6 +2,6 @@ class newfile
 {
     public static void main(String[] args)
     {
-        System.out.println("This Is Mypractice  Project");
+        System.out.println("This Is My practice  Project1");
     }
 }
